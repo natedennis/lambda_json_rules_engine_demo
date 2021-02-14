35f68ec25220f2c7bb0f10909be9dec3d67a9408
Nathan Dennis <ndennis@surgeforward.com>
2021-02-13 20:24:49 -0500

    cleaning up a bit

--- handler.js
+++ handler.js
@@ -18,17 +18,6 @@ module.exports.hello = async (event) => {
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
 
-
-module.exports.app = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'AppDividend API is accessed',
-      input: event,
-    }),
-  };
-};
-
 module.exports.rules = async (event, context, callback) => {
   var person = JSON.parse(event.body);
   
