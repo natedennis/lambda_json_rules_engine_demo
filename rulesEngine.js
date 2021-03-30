e87116576d2b45b510ea94580d42a830783155dd
Nathan Dennis <ndennis@surgeforward.com>
2021-03-30 15:27:21 -0400

    cache rules engine instantiation

--- rulesEngine.js
+++ rulesEngine.js
@@ -2,57 +2,62 @@ require('colors');
 
 const { Engine } = require('json-rules-engine');
 
-module.exports.jsonRules = async function jsonRules( person) {
 
-    /**
-     * Setup a new engine
-     */
-    let engine = new Engine();
+/**
+ * Setup a new engine
+ */
 
-    //add some rules
-    for (i = 0; i < 10000; i++) {
-        console.log('add rule: ' + i);
-        const rule = {
-            conditions: {
-                any: [{
-                    fact: 'age',
-                    operator: 'greaterThanInclusive',
-                    value: i
-                }]
-            },
-            event: {  // define the event to fire when the conditions evaluate true
-                type: 'age',
-                params: {
-                    message: 'Person is at least ' + i
-                }
+let addRulesTime = Date.now();
+
+let engine = new Engine();
+//add some rules
+for (i = 0; i < 10000; i++) {
+    const rule = {
+        conditions: {
+            any: [{
+                fact: 'age',
+                operator: 'greaterThanInclusive',
+                value: i
+            }]
+        },
+        event: {  // define the event to fire when the conditions evaluate true
+            type: 'age',
+            params: {
+                message: 'Person is at least ' + i
             }
-        };
-        console.log('rule added:\n', rule);
+        }
+    };
+    engine.addRule(rule);
+}
+console.log('Init Engine and Add Rules time : ' + (Date.now()-addRulesTime) + ' ms');
+module.exports.jsonRules = async function jsonRules(person) {
 
-        engine.addRule( rule );
-    }
-
-
-
+    let addFactsTime = Date.now();
     engine.addFact('age', person.age);
-
     engine.addFact('name', person.name);
+    console.log('Add facts time : ' + (Date.now()-addFactsTime) + ' ms');
 
-
-
-    engine.on('success', function (event, almanac, ruleResult) {
-        console.log('Success event:\n', event);
-    });
+//    engine.on('success', function (event, almanac, ruleResult) {
+//        console.log('Success event:\n', event);
+//    });
 
     // Run the engine to evaluate
     let fired = [];
- 
-    const {events} = await engine
+
+    let engineRunTime = Date.now();
+    const { events } = await engine
         .run()
     events.map(event => {
         console.log(event.params.message);
         fired.push(event.params.message);
     })
     person.fired = fired;
-    return  person;
+    console.log('Engine Run time : ' + (Date.now()-engineRunTime) + ' ms');
+
+    let removeFactTime = Date.now();
+    engine.removeFact('age');
+    engine.removeFact('name');
+    console.log('Engine remove facts time : ' + (Date.now()-removeFactTime) + ' ms');
+
+    return person;
 }
